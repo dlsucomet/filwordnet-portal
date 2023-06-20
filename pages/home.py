a9aaf30c501e3ad308de9a25a9d7d9d714f3ae75
unknown <phoebs_ong@dlsu.edu.ph>
2023-06-20 19:45:12 +0800

    Rename 'Networks' to 'Word Occurence'

--- pages/home.py
+++ pages/home.py
@@ -197,7 +197,7 @@ edges = [
 elements = nodes + edges
 
 network = dbc.Row([
-    html.H4('Network'),
+    html.H4('Word Occurence'),
     html.Div([
         cyto.Cytoscape(
             layout={'name': 'concentric'},
