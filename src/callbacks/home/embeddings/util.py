8c9d62a37844df5e275b2aa1159b6ab94efac968
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-11-04 22:22:47 +0800

    Remove unneeded embeddings function

--- src/callbacks/home/embeddings/util.py
+++ src/callbacks/home/embeddings/util.py
@@ -1,11 +1,1 @@
-from sklearn.decomposition import PCA
 
-
-def load_embeddings(embeddings_list):
-    if embeddings_list:
-
-        pca = PCA(n_components=3)
-
-        return pca.fit_transform(embeddings_list)
-
-    return ''
